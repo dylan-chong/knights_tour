592adc48677768904d549497a476c66bc486ddbc
Dylan Chong <dylan.chong@icloud.com>
2017-09-29 09:30:58 +1300

    Added board

---
+++ lib/board.ex
@@ -0,0 +1,66 @@
+defmodule Board do
+  @moduledoc """
+  A Chess Board
+  """
+
+  @enforce_keys [:width, :height]
+  defstruct [:width, :height, map: %{}]
+
+  def put(%Board{width: width, height: height}, x, y, _)
+      when x not in 0..width - 1
+      or y not in 0..height - 1
+  do
+    raise ArgumentError, message: "Position out of bounds: {#{x}, #{y}}"
+  end
+
+  def put(board = %Board{}, x, y, item) do
+    %Board{board |
+      map: board.map |> Map.put(
+        {x, y}, item
+      )
+    }
+  end
+
+  def get(%Board{width: width, height: height}, x, y)
+      when x not in 0..width - 1
+      or y not in 0..height - 1
+  do
+    raise ArgumentError, message: "Position out of bounds: {#{x}, #{y}}"
+  end
+
+  def get(board = %Board{}, x, y) do
+    board.map[{x, y}]
+  end
+
+  def to_string(board = %Board{}) do
+    (
+      for y <- 0..board.width - 1,
+      do: Enum.join([
+        row_to_string(board, y),
+        edge_row(board),
+      ], "\n")
+    )
+    |> List.insert_at(0, edge_row(board))
+    |> Enum.join("\n")
+  end
+
+  defp edge_row(%Board{width: width}) do
+    (for _ <- 0..width - 1, do: "-----+")
+    |> List.insert_at(0, "+")
+    |> Enum.join("")
+  end
+
+  defp row_to_string(board = %Board{}, y) do
+    (
+      for x <- 0..board.width - 1,
+      do: String.pad_trailing(
+        " #{board |> Board.get(x, y)}",
+        5
+      )
+      |> Kernel.<>("|")
+    )
+    |> List.insert_at(0, "|")
+    |> Enum.join("")
+  end
+
+end
---
+++ test/board_test.exs
@@ -0,0 +1,43 @@
+defmodule BoardTest do
+  use ExUnit.Case
+  doctest Board
+
+  test "to_string should not crash" do
+    string =
+      %Board{width: 8, height: 8}
+      |> Board.put(4, 4, :a)
+      |> Board.put(3, 2, :b)
+      |> Board.put(0, 0, :c)
+      |> Board.put(7, 7, :d)
+      |> Board.to_string
+    assert is_binary(string)
+  end
+
+  test "get and put should store and retrieve values" do
+    board = %Board{width: 8, height: 8}
+
+    assert Board.get(board, 1, 0) == nil
+
+    value =
+      board
+      |> Board.put(1, 0, :value)
+      |> Board.get(1, 0)
+
+    assert value == :value
+  end
+
+  test "put out of range should raise" do
+    assert_raise ArgumentError, fn ->
+      %Board{width: 8, height: 8}
+      |> Board.put(-1, -1, :value)
+    end
+  end
+
+  test "get out of range should raise" do
+    assert_raise ArgumentError, fn ->
+      %Board{width: 8, height: 8}
+      |> Board.get(-1, -1)
+    end
+  end
+
+end
