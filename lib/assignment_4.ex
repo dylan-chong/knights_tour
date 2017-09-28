68a605f367c7183e7b0608d674fba3e6febb0f37
Dylan Chong <dylan.chong@icloud.com>
2017-09-28 20:43:02 +1300

    Base project

---
+++ lib/assignment_4.ex
@@ -0,0 +1,18 @@
+defmodule Assignment4 do
+  @moduledoc """
+  Documentation for Assignment4.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Assignment4.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/assignment_4_test.exs
@@ -0,0 +1,8 @@
+defmodule Assignment4Test do
+  use ExUnit.Case
+  doctest Assignment4
+
+  test "greets the world" do
+    assert Assignment4.hello() == :world
+  end
+end
