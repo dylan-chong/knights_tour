32aea4c927d7b87a5c953490f440cba23684a20c
Dylan Chong <dylan.chong@icloud.com>
2017-10-08 17:36:55 +1300

    Add timer for bench_increasing_square

--- lib/timer.ex
+++ lib/timer.ex
@@ -1,24 +1,53 @@
 defmodule Timer do
 
-  def bench(solver, width, height) do
-    bench(solver, %Board{width: width, height: height})
+  def bench_increasing_square(
+    solver,
+    min \\ 6,
+    max \\ 60,
+    interval \\ 6
+  ) do
+    (
+      for x <- min..max,
+      rem(x, interval) == 0,
+      x != 42,
+      do: x
+    )
+    |> Enum.map(fn size ->
+      r = bench_square(solver, size, false)
+      IO.puts "Finished for size: #{size}, millis: #{elem(r, 0)}"
+      r
+    end)
   end
 
-  def bench(solver, board = %Board{}) do
-    {millis, result} = measure(
-      fn -> solver.solve(board) end
-    )
+  def bench_square(solver, size, print \\ true) when is_number(size) do
+    bench_size(solver, size, size, print)
+  end
 
-    IO.puts "* Duration: #{millis} ms"
-    if result == :no_closed_tour_found do
-      IO.puts "  * Result: :no_closed_tour_found"
-    else
-      IO.puts "  * Result:"
-      result
-      |> Keyword.fetch!(:board)
-      |> Board.to_string
-      |> IO.puts
+  def bench_board(solver, board = %Board{}, print \\ true) do
+    measure_result =
+      {millis, result} =
+        measure(fn ->
+          solver.solve(board)
+        end)
+
+    if print do
+      IO.puts "* Duration: #{millis} ms"
+      if result == :no_closed_tour_found do
+        IO.puts "  * Result: :no_closed_tour_found"
+      else
+        IO.puts "  * Result:"
+        result
+        |> Keyword.fetch!(:board)
+        |> Board.to_string
+        |> IO.puts
+      end
     end
+
+    measure_result
+  end
+
+  def bench_size(solver, width, height, print \\ true) do
+    bench_board(solver, %Board{width: width, height: height}, print)
   end
 
   def measure(prepare_function, work_function) do
